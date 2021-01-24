d8e3023a9ee7710aaa2db308dd87e4821d87ec1a
Shanen Cross <shanencross@yahoo.com>
2021-01-24 15:07:16 -0800

    add hello removal functionality
    Co-authored by: Collin Chapman <cchap14@gmail.com>
    Co-authored by: Darien West <dwestcodes@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
 	$("button#hello").click(function() {
 		$("ul#user").prepend("<li>Hello!</li>");
 		$("ul#webpage").prepend("<li>Why hello there!</li>");
+		
+		$("ul#user").children("li").first().click(function() {
+			$(this).remove();
+		});
+		$("ul#webpage").children("li").first().click(function() {
+			(this).remove();
+		});
 	});
 
 	$("button#goodbye").click(function() {
