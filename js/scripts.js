59a252c97456884a01100a69b782f833edd0fd2b
Shanen Cross <shanencross@yahoo.com>
2021-01-24 14:31:22 -0800

    Fix typo for hello message

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$("button#hello").click(function() {
-		$("ul").prepend("<li>Goodbye!</li>");
+		$("ul").prepend("<li>Hello!</li>");
 	});
 
 	$("button#goodbye").click(function() {
